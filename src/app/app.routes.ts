9765aff4b8e886900810c4f521551c0723098511
elbra <elbrac.94@gmail.com>
2025-06-10 20:00:45 -0500

    feat: Version inicial Gestión Completa de Recordatorios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,10 @@ export const routes: Routes = [
     loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
   },
   {
+    path: 'recordatorios',
+    loadComponent: () => import('./recordatorios/recordatorios.page').then((m) => m.RecordatoriosPage),
+  },
+  {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardContent,
+  IonButton,
+  IonIcon
+} from '@ionic/angular/standalone';
+import { RouterModule } from '@angular/router';
+import { addIcons } from 'ionicons';
+import { notifications } from 'ionicons/icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardContent,
+    IonButton,
+    IonIcon,
+    RouterModule
+  ],
 })
 export class HomePage {
-  constructor() {}
+  constructor() {
+    addIcons({ notifications });
+  }
 }
